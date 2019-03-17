1cf33880fcd4def9f02e6d6eb05f36e0a74454bd
Berkan Korkmaz <korkmazaberkan@gmail.com>
2019-03-17 17:57:26 +0300

    kurulum

---
+++ MVCFilmSatis/Controllers/TranslateController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFilmSatis.Controllers
+{
+    public class TranslateController : Controller
+    {
+        // GET: Translate
+        public ActionResult Index(string id, string url)
+        {
+            HttpCookie langCookie = new HttpCookie("lang");
+            langCookie.Value = id;
+            Response.Cookies.Add(langCookie);
+            
+            return Redirect(url);
+        }
+    }
+}
---
+++ MVCFilmSatis/Controllers/YoungController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFilmSatis.Controllers
+{
+    public class YoungController : Controller
+    {
+        // GET: Young
+        [OnlyYoungAndAdmin] 
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
